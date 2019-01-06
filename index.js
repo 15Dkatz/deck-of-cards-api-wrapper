b68f3a590eda9ed45fbc2dcdb55be2451c460fe2
David Katz <15Dkatz@shcp.edu>
2019-01-06 11:34:53 -0800

    Add Rate Limiting middleware

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
+const limitingMiddleware = new LimitingMiddleware();
+
+app.use(limitingMiddleware.limitByIp());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
@@ -28,6 +32,14 @@ app.get('/deck/:deck_id/draw', (req, res) => {
   }, (error, response, body) => {
     res.json(JSON.parse(body));
   });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error', message: err.message
+  })
 });
 
 const PORT = process.env.PORT || 3000;
